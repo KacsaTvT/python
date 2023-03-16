0d7b4a40406d3b8a62d47ac56fab00b34812f351
KacsaTvT <kacsatvt@gmail.com>
2023-03-16 19:07:00 +0100

    Update kiir2.py

--- sulipy/kiir2.py
+++ sulipy/kiir2.py
@@ -1,11 +1,4 @@
 #Írj egy programot, amely a felhasználótól kér be egész számokat [-5;5] intervallumban. A bekérés akkor fejeződjön be, amikor a felhasználó intervallumon kívüli értéket ad meg! A program írja ki a megadott intervallumba eső számokat és az összegüket.
-"""
-szamok = 0
-while -5 <= szamok <= 5:
-    szamok = int(input("Írj be egy számot 5 és -5 között: "))
-    if -5 <= szamok <= 5:
-        print(szamok)
-"""
 szamok_ossz = 0
 szamok = 0
 while -5 <= szamok <= 5:
