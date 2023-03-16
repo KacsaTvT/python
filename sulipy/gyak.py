a9d347ac848a0415731d9476c8c892db6e2d9e14
KacsaTvT <kacsatvt@gmail.com>
2023-03-16 19:15:02 +0100

    kiir2

--- sulipy/gyak.py
+++ sulipy/gyak.py
@@ -1,9 +1,15 @@
 #Készíts egy programot, amely a felhasználó által megadott mondatról a mondatvégi jel alapján eldönti milyen fajtájú! (kijelentő, kérdő, felkiáltó / felszólító / óhatjtó)
-vizsgal=input("Írj egy mondatot! ")
-for jel in vizsgal:
-    if jel=="?":
-        print("Ez egy kérdés")
-    if jel==".":
-        print("Ez egy kijelentés")
-    if jel=="!":
-        print("Ez egy felkiáltó / felszólító / óhatjtó")
+folyt=True
+vizsgal=None
+while folyt:
+    while vizsgal!="":
+        vizsgal=input("Írj egy mondatot! ")
+        for jel in vizsgal:
+            if jel=="?":
+                print("Ez egy kérdés")
+            if jel==".":
+                print("Ez egy kijelentés")
+            if jel=="!":
+                print("Ez egy felkiáltó / felszólító / óhatjtó")
+    while vizsgal!="":
+        folyt=False
