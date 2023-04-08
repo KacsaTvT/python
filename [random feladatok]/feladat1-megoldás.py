214e6893f61c7b748f87c0c2dac79a052101b3ca
KacsaTvT <kacsatvt@gmail.com>
2023-04-08 21:16:27 +0200

    commentek

--- [random feladatok]/feladat1-megoldás.py
+++ [random feladatok]/feladat1-megoldás.py
@@ -1,9 +1,8 @@
-valaszok=[]
-v1=float(input("Adj meg egy számot"))
-v2=float(input("Adj meg mégegy számot"))
-print("Az első számod: ",v1)
-print("A második számod: ", v2)
-print(v1+v2)
-print(v1-v2)
-print(v1*v2)
-print(v1/v2)
+v1=float(input("Adj meg egy számot")) #első szám bekérése
+v2=float(input("Adj meg mégegy számot")) #második szám bekérése
+print("Az első számod: ",v1) #első szám kiírása
+print("A második számod: ", v2) #második szám kiírása
+print(v1+v2) #számok összege
+print(v1-v2) #számok különbsége
+print(v1*v2) #számok szorzata
+print(v1/v2) #számok hányadosa
